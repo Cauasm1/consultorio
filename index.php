acef98de921cbd88b2ab57769955f1bcade3e733
Cauasm1 <cauam2498@gmail.com>
2024-06-18 22:26:45 -0300

    Consultorio

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>Consultorio Dr. Alguém</h1>
+
+        <naigation>
+            <a id="primeiro" class="button" role="button" href="index.php">Inicio</a>
+            <a class="button" role="button" href="cadastro.php">Cadastro</a>
+            <a class="button" role="button" href="">Sobre</a>
+            <a class="button" role="button" href="">Contato</a>
+        </naigation>
+    </header>
+
+    <div class="img">
+
+    </div>
+
+    <footer>
+        Direitos autorais por Cauã
+    </footer>
+
+</body>
+
+</html>
